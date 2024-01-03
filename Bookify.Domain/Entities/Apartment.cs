a84c5f4857d7dffb6314b018caab569e10d0be2a
David Henley <github@drh.dev>
2024-01-03 14:52:54 -0600

    Add Entity abstraction

--- Bookify.Domain/Entities/Apartment.cs
+++ Bookify.Domain/Entities/Apartment.cs
@@ -1,15 +1,21 @@
+using Bookify.Domain.Abstractions;
+
 namespace Bookify.Domain.Entities;
 
-public sealed class Apartment
+public sealed class Apartment : Entity
 {
-    public Guid Id { get; private set; }
+    public Apartment(Guid id)
+        : base(id)
+    {
+    }
+
     public string Name { get; private set; }
     public string Description { get; private set; }
-    public string Country { get; private set;  }
-    public string State { get; private set;  }
-    public string ZipCode { get; private set;  }
-    public string City { get; private set;  }
-    public string Street { get; private set;  }
+    public string Country { get; private set; }
+    public string State { get; private set; }
+    public string ZipCode { get; private set; }
+    public string City { get; private set; }
+    public string Street { get; private set; }
     public decimal PriceAmount { get; private set; }
     public string PriceCurrency { get; private set; }
     public decimal CleaningFeeAmount { get; private set; }
